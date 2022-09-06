b36762d5150fafa34ece386cdec0b5a80dfbedf7
ALEJO-29 <alejandro123aut@gmail.com>
2022-09-06 16:29:53 -0500

    Update

--- reto2022/registroApp/models.py
+++ reto2022/registroApp/models.py
@@ -18,20 +18,19 @@ class Ciudadano(models.Model):
     dire = models.CharField(max_length=30)
     barrio = models.CharField(max_length=30)
     fecha_nac = models.DateField(auto_now_add=False)
-<<<<<<< HEAD
-    etnia = models.models.CharField(max_length=30)
-    disc = models.CharField(max_length=30)
-    estrato = models.models.IntegerField()
-    accs_tec = models.CharField(max_length=30)
-=======
-    etnia = models.CharField( max_length=30)
-    disc = models.CharField(max_length=30)
-    estrato = models.IntegerField()
-    accs_tec= models.CharField(max_length=30)
->>>>>>> cf9b17a46981faf7be3c52ae19d82035a544e60b
-    cuales = models.CharField(max_length=50)
-    con_int = models.BooleanField()
-    reg = models.CharField(max_length=30)
+
+
+etnia = models.models.CharField(max_length=30)
+disc = models.CharField(max_length=30)
+estrato = models.models.IntegerField()
+accs_tec = models.CharField(max_length=30)
+etnia = models.CharField(max_length=30)
+disc = models.CharField(max_length=30)
+estrato = models.IntegerField()
+accs_tec = models.CharField(max_length=30)
+cuales = models.CharField(max_length=50)
+con_int = models.BooleanField()
+reg = models.CharField(max_length=30)
 
 
 class Pregunta(models.Model):
@@ -51,22 +50,20 @@ class Sondeo (models.Model):
     visible = models.BooleanField(default=True)
     tematica = models.CharField(max_length=25)
     fecha_c = models.DateField(auto_now=False, auto_now_add=False)
-<<<<<<< HEAD
-    hora_c = models.TimeField(auto_now=False, auto_now_add=False)
-    img = models.ImageField()
-=======
-    hora_c =models.TimeField( auto_now=False, auto_now_add=False)
-    img = models.ImageField(upload_to="sondeo/")
-    updated = models.DateTimeField( auto_now_add=True)    
-    id_pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
-    id_condicion = models.ForeignKey(Condicion, on_delete=models.CASCADE)
-    id_ciudadano = models.ManyToManyField(Ciudadano)
-                            
-        
+
+
+hora_c = models.TimeField(auto_now=False, auto_now_add=False)
+img = models.ImageField()
+hora_c = models.TimeField(auto_now=False, auto_now_add=False)
+img = models.ImageField(upload_to="sondeo/")
+updated = models.DateTimeField(auto_now_add=True)
+id_pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+id_condicion = models.ForeignKey(Condicion, on_delete=models.CASCADE)
+id_ciudadano = models.ManyToManyField(Ciudadano)
+
+
 class Certificado(models.Model):
     fecha_gen = models.DateTimeField(auto_now_add=True)
     id_sondeo = models.ForeignKey(Sondeo, on_delete=models.CASCADE)
-    
-    
-    
->>>>>>> cf9b17a46981faf7be3c52ae19d82035a544e60b
+
+

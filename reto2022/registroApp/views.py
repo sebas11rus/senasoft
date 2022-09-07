4e4a70264a1d1e33dd145df99c53caf9870576d3
ALEJO-29 <alejandro123aut@gmail.com>
2022-09-07 08:25:04 -0500

    update

--- reto2022/registroApp/views.py
+++ reto2022/registroApp/views.py
@@ -9,8 +9,9 @@ def registro(request):
     form = RegistroForm()
     if request.method == "POST":
         ciudadano = Ciudadano(
-            tipo_doc=request.POST["tipo_doc"], n_doc=request.POST["n_doc"], nom=request.POST["nom"]
+            tipo_doc=request.POST["tipo_doc"], n_doc=request.POST["n_doc"], nom=request.POST["nom"],
             ape=request.POST["ape"],sexo=request.POST["sexo"], cel=request.POST["cel"], tel=request.POST["tel"],
-            mun=request.POST["mun"],dire=request.POST["dire"],)
+            mun=request.POST["mun"],dire=request.POST["dire"], barrio=request.POST["barrio"], fecha_nac=request.POST["fecha_nac"],
+            etnia=request.POST["etnia"], disc=request.POST["disc"],estrato=request.POST["estrato"])
 
     return render(request, 'registro.html', {'form': form})

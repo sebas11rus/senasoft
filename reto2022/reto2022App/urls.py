97ef1b94b2b86de3a0ea01b8fa3add94d2dadab0
ALEJO-29 <alejandro123aut@gmail.com>
2022-09-08 10:33:28 -0500

    update

--- reto2022/reto2022App/urls.py
+++ reto2022/reto2022App/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contestar/', views.contestar, name='contestar'),
     path('', views.error, name='error'),
+    path('reporte/', views.Generar_pdf.as_view(), name='reporte'),
 ]
--- reto2022/reto2022App/views.py
+++ reto2022/reto2022App/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render
-from registroApp.models import Sondeo,Pregunta
+from django.shortcuts import render, HttpResponse
+from registroApp.models import Sondeo, Pregunta
+from .proccess import html_pdf
+from django.views.generic import View
+
+
 # Create your views here.
 
 def index(request):
     sondeos = Sondeo.objects.all()
-    return render(request,'index.html', {'sondeos': sondeos})
+    return render(request, 'index.html', {'sondeos': sondeos})
+
 
 def contestar(request):
     sondeos = Sondeo.objects.filter(visible=True, )
     return render(request, 'contestar.html', {'sondeos': sondeos})
 
+
 def error(request):
     return render(request, 'error.html')
 
-def pregunta(request,id):
-    pregunta= Pregunta.objects.filter(request=id)
+
+def pregunta(request, id):
+    pregunta = Pregunta.objects.filter(request=id)
     return render(request, 'pregunta.html')
 
 
+# vista basada en clase
+class Generar_pdf(View):
+
+    def get(self, request):
+        # traer el template
+        pdf = html_pdf('reporter.html')
+
+        # redige el html
+        return HttpResponse(pdf, content_type='application/pdf')
